0ae41ad48a501098adf94905b0134a05bc578527
romanf <fomin.tp21@mail.ru>
2022-01-19 17:57:43 +0300

    Написал  функцию, которая добавляет в массив новое число Фибоначчи

--- 2L_FominRoman/main.swift
+++ 2L_FominRoman/main.swift
@@ -46,3 +46,35 @@ while i < array.count {
 }
 print("Удаляем из массива все четные числа и все числа, которые не делятся на 3: ")
 print(array)
+
+
+
+//5. * Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 100 элементов.
+// Числа Фибоначчи определяются соотношениями Fn=Fn-1 + Fn-2.
+
+// на входе  в функцию - кол-во элементов в ряду
+// на выходе из функции - массив из чисел Фибоначчи
+
+func fibonacci (numberOfElements: Int) -> [UInt64] {
+    var arrayFibonacci: [UInt64] = [] // объявляем пустой массив, элементы максимальной числовой размерности
+    arrayFibonacci.append(0) // нулевой элемент присвоить  0
+    arrayFibonacci.append(1) // первый элемент присвоить  1
+    var element: UInt64 = 0
+    var numberOfElements1 = numberOfElements
+    
+    if numberOfElements > 93 {
+        print("К сожалению, не могу вывести 100 элементов, есть ограничения на разрядность чисел в языке SWIFT. Произведу расчет только до 94 элемента")
+        numberOfElements1 = 93
+    }
+    
+    for i in 2...numberOfElements1 {
+        element = arrayFibonacci [i-2] + arrayFibonacci [i-1]
+        arrayFibonacci.append(element)
+    }
+    
+    return arrayFibonacci
+}
+
+var arr = fibonacci(numberOfElements: 100)
+print("Массив из чисел Фибоначчи: ")
+print(arr)
